2cae368861b819833452ab9a5a54f380846ce1d7
buingoclam <lb130801@gmail.com>
2022-07-18 14:11:11 +0700

    convension login

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <header class="main-header">
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="logo-container">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="{{ route('home') }}">
                 <img class="logo-image" src="{{ asset('images/logo.png') }}" alt="HapoLearn Logo">
             </a>
             <div class="navbar-icons">
@@ -13,13 +13,13 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto w-100">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="{{ route('home') }}">HOME <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="login">ALL COURSES</a>
+                    <a class="nav-link" href="#">ALL COURSES</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="register" aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link" href="{{ route('login') }}" aria-haspopup="true" aria-expanded="false">
                         LOGIN/REGISTER
                     </a>
                 </li>
